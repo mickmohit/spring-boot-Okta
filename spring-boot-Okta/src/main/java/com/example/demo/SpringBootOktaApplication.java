5dd14505f867d316b10e0ad0985682a0f8d651b8
Mohit Darmwal <36398566+mickmohit@users.noreply.github.com>
2018-05-22 20:04:12 +0530

    Add files via upload

---
+++ spring-boot-Okta/src/main/java/com/example/demo/SpringBootOktaApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOktaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOktaApplication.class, args);
+	}
+}
